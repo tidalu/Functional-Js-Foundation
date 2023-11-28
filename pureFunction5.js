0484fe6ab080c238c74fbf342ce5785134cc928e
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-28 23:13:19 +0100

    pure functions: principles

---
+++ pureFunction5.js
@@ -0,0 +1,5 @@
+// functions as tiny units to be combined and run automatically must be highly predictable 
+
+// We rely on using their evaluated result to pass the input to the next
+// unit of code (automatically). Any ‘side effects’ would destroy this
+
