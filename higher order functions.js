500d5416182b2f62514c47262d9f7e49e6bd7c16
Tidalu <ul0271069@edu.uni.lodz.pl>
2023-11-28 23:12:50 +0100

    higher order functions: reduce, map, filter

---
+++ higher order functions.js
@@ -0,0 +1,11 @@
+// functions in js == First class objects
+
+// they can be treated like any other js objects
+
+
+// higher order functions : takes in a function or passes out 
+
+
+// just a term to describe these functions - any function that does it we call that - but there is nothing different about them inheritly
+
+// functional
